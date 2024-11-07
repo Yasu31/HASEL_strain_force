21b303f114d0d342d23aab35df1d8bbf4e4a3292
Yasunori Toshimitsu <yasu313nori@gmail.com>
2024-11-07 16:59:14 +0100

    use numForceSteps rather than specify the values of the forces directly: better suited for handling logarithmic force application

--- strainForceAll.m
+++ strainForceAll.m
@@ -12,27 +12,10 @@ clear
 % FName = dir('SF_*_string_05Hz_6kV_Ramp*.txt') % comparison [string type] in ramp
 % FName = dir('SF_*_string_05Hz_6kV_Step*.txt') % comparison [string type] in step
 FName = dir('test_05Hz_7kV_Rampedsquare_35kVs_2022_11_14_1253.txt') 
-%% etc.
-
-% FName(1).name = '10x90_new_05Hz_7kV_Rampedsquare_25kVs_2022_05_11_1140.txt';
-% FName(2).name = '10x90_old_05Hz_7kV_Rampedsquare_25kVs_2022_05_11_1202.txt';
-% FName(3).name = '20x60_new_05Hz_7kV_Rampedsquare_25kVs_2022_05_11_1245.txt';
-% FName(4).name = '20x60_old_05Hz_7kV_Rampedsquare_25kVs_2022_05_11_1223.txt';
-
-% FName(1).name = '10x60_EUP_FR3_00_05Hz_7kV_Rampedsquare_25kVs.txt';
-% FName(2).name = '20x60_EUP_FR3_00_05Hz_7kV_Rampedsquare_25kVs.txt';
-% FName(3).name = '10x60_EUP_FR3_85_05Hz_7kV_Rampedsquare_25kVs.txt';
-% FName(4).name = '20x60_EUP_FR3_85_05Hz_7kV_Rampedsquare_25kVs.txt';
-
-% FName(1)s.name = '20x60_EUP_FR3_00_05Hz_7kV_Stepinput.txt';
-% FName(2).name = '20x60_EUP_FRB_00_05Hz_10kV_Stepinput.txt';
-% FName(1).name = '20x60_EUP_FR3_00_05Hz_7kV_Rampedsquare_25kVs.txt';
-% FName(2).name = '20x60_EUP_FRB_00_05Hz_10kV_Rampedsquare_36kVs.txt';
 
 %% experimental parameters
-
 exPara.sampleFreq = 1000;
-exPara.forceStep = 2; %N
+exPara.numForceSteps = 26; % including zero
 exPara.forceMax = 50; %N
 exPara.voltMax = 7; %kV
 exPara.inputFreq = 0.5;
@@ -42,8 +25,8 @@ exPara.kD = 1.93; %mm/V
 exPara.rampTime_step = 0; %kV/s for step input
 exPara.rampTime_ramp = 7/35; %kV/(kV/s) for ramp input
 exPara.holdTime = 1/3; % 1/3 of the cycle: hold off ->  ramp up -> hold on -> ramp down -> off till cycle end
-%% HASEL parameters
 
+%% HASEL parameters
 HaPara.width_pouch = 90e-3;
 HaPara.height_pouch = 10e-3;
 HaPara.number_pouch = 14;
@@ -51,18 +34,12 @@ HaPara.epsilon_film = 3.2; %Dielectric constant of film / Mylar 3.2, BOPP 2.8
 HaPara.epsilon_0 = 8.85 *10^-12;
 HaPara.thickness_film = 15*10^-6; %um / Mylar 15um, BOPP 20um
 %% raw data plot
-
-
-TargetForce = [0: exPara.forceStep: exPara.forceMax];
-
-
 smoothingFactor = exPara.sampleFreq/10;
-numForceSteps = exPara.forceMax/exPara.forceStep + 1;
 
-Data(1: length(FName), 1: numForceSteps) =...
+Data(1: length(FName), 1: exPara.numForceSteps) =...
     struct('strain_raw', zeros(exPara.inputRepeat, 1), 'strain', 0, 'force', 0);
 for i = 1: length(FName)
-    fName = FName(i).name
+    fName = FName(i).name;
     if contains(fName, 'Step')
         exPara.rampTime = exPara.rampTime_step;
     elseif contains(fName, 'Ramp')
@@ -100,50 +77,45 @@ for i = 1: length(FName)
     
         
     
-    for j = 1: numForceSteps + 1
-        if length(Time) < j * exPara.sampleFreq * exPara.inputRepeat / exPara.inputFreq
-            disp(['end at', num2str((j-1-1)*exPara.forceStep), 'N: not enough data length'])
+    for forceStep = 1: exPara.numForceSteps + 1
+        if length(Time) < forceStep * exPara.sampleFreq * exPara.inputRepeat / exPara.inputFreq
+            disp(['end at force step ', num2str(forceStep), ': not enough data length'])
             break
         end
-        Data(i, j) = detectStrain_cnv(Time, Force_fil, Disp_fil, (j-1)*exPara.forceStep, exPara, 0);
+        Data(i, forceStep) = detectStrain_cnv(Time, Force_fil, Disp_fil, forceStep, exPara, 0);
     end
     
     
     
     title([fName(1:4), ' ', fName(11:11), 'kV '])
-    for j = 1: numel(TargetForce)
-        targetForce = TargetForce(j);
-        idx0 = fix(targetForce/exPara.forceStep*1/exPara.inputFreq*exPara.inputRepeat*exPara.sampleFreq) + 1;
+    for forceStep = 1: exPara.numForceSteps + 1
+        idx0 = fix((forceStep - 1)/exPara.inputFreq*exPara.inputRepeat*exPara.sampleFreq) + 1;
         idx1 = idx0 - 1 + fix(1/exPara.inputFreq*exPara.inputRepeat*exPara.sampleFreq);
         
         if length(Time) < idx1
-            disp(['end at', num2str(TargetForce(j-1)), 'N'])
+            disp(['end at force step ', num2str(forceStep)])
             break
         end
 
-        figure(100*i+j)
+        figure(100*i+forceStep)
         clf
         hold on
         
         yyaxis('left')
         plot(Time(idx0:idx1), Volt(idx0:idx1))
-        %     plot(Time, RefFreq)
         ylabel('Voltage [kV]')
         xlabel('time [s]')
-        %     legend('real input', 'ref input for TF', 'output', "Location", "northoutside", "Orientation","horizontal")
         
         yyaxis('right')
         plot(Time(idx0:idx1), Disp(idx0:idx1))
         plot(Time(idx0:idx1), Disp_fil(idx0:idx1), 'k-', 'LineWidth', 2)
         
-        %         ylim([-1.0, 1.0])
         ylabel('displacement [mm]')
         grid on
-        %         xlim([Time(idx0) - 5/targetFreq, Time(idx0) + 5/targetFreq])
         grid on
-        title([num2str(targetForce), 'N'])
+        title(['Force Step #', num2str(forceStep)])
         
-        detectStrain_cnv(Time, Force_fil, Disp_fil, targetForce, exPara, 1);
+        detectStrain_cnv(Time, Force_fil, Disp_fil, forceStep, exPara, 1);
         
     end
     
@@ -195,51 +167,12 @@ function Out = correctTimedelay(Data)
 Out = [Data(2:end); Data(end)];
 end
 
-function Out = detectStrain(Time, Force, Disp, targetForce, exPara, flagPlot)
-samplesPerVolt = exPara.sampleFreq/exPara.inputFreq; % num samples per volt
-samplesPerForceStep = samplesPerVolt * exPara.inputRepeat; % num samples per displacement step
-
-
-i = targetForce/exPara.forceStep + 1;
-startIndex = (i - 1)*samplesPerForceStep + 1;
-endIndex = i*samplesPerForceStep;
-
-Out.force = -mean(Force(startIndex: endIndex));
-
-Ind_max = zeros(exPara.inputRepeat, 1);
-Ind_min = zeros(exPara.inputRepeat, 1);
-Val_max = zeros(exPara.inputRepeat, 1);
-Val_min = zeros(exPara.inputRepeat, 1);
-
-for j = 1: exPara.inputRepeat
-    startIndex2 = startIndex + (j - 1)*samplesPerVolt;
-    midIndex2 = startIndex2 + (1/2)*samplesPerVolt;
-    endIndex2 = (startIndex - 1) + j*samplesPerVolt;
-    [Val_max(j), Ind_max(j)] = max(Disp(startIndex2: midIndex2));
-    [Val_min(j), Ind_min(j)] = min(Disp(midIndex2: endIndex2));
-    
-    Ind_max(j) = Ind_max(j) + startIndex2 - 1;
-    Ind_min(j) = Ind_min(j) + midIndex2 - 1;
-    
-    if flagPlot
-        yyaxis('right')
-        plot(Time(Ind_min(j)), Disp(Ind_min(j)), 'bo', "LineWidth", 2)
-        plot(Time(Ind_max(j)), Disp(Ind_max(j)), 'ro', 'LineWidth', 2)
-    end
-end
-Out.strain_raw = Val_max - Val_min;
-Out.strain = mean(Out.Strain_raw);
-
-end
-
-
-function out = detectStrain_cnv(Time, Force, Disp, targetForce, exPara, flagPlot)
+function out = detectStrain_cnv(Time, Force, Disp, forceStep, exPara, flagPlot)
 
 spc= exPara.sampleFreq/exPara.inputFreq;
 spf = spc * exPara.inputRepeat; % num samples per displacement step
 
-i = targetForce/exPara.forceStep + 1;
-startIndex = (i - 1)*spf + 1;
+startIndex = (forceStep - 1)*spf + 1;
 % endIndex = i*samplesPerForceStep;
 
 max_stroke = zeros(exPara.inputRepeat, 1);
@@ -275,7 +208,6 @@ for j = 1: exPara.inputRepeat
 end
 out.strain_raw = rawStroke;
 out.strain = mean(rawStroke);
-% out.force = targetForce;
 out.force = mean(force);
 
 
